f9948226133d8d9df856ecb14430eeb6d3d686e2
valyakuttan <valyakuttan@noreply.com>
2019-01-18 14:23:41 +0530

    Updates

---
+++ precache-manifest.dba2392f177f090201ba9cb329f30db9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "13fabb82119390db5bb5",
+    "url": "/webapp/static/css/main.bded75db.chunk.css"
+  },
+  {
+    "revision": "13fabb82119390db5bb5",
+    "url": "/webapp/static/js/main.13fabb82.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/webapp/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "65c26c62aa662cc6acef",
+    "url": "/webapp/static/js/runtime~main.65c26c62.js"
+  },
+  {
+    "revision": "ce644a350714550b3b47036fb89add79",
+    "url": "/webapp/index.html"
+  }
+];
